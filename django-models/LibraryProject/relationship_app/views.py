a574f6ddcb8a3aed2d3f84a187ab5cd1ca20405d
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 13:36:43 +0100

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
 # Create your views here.
+
+def book_list(request):
+    #A basic function view that lists all books stored in the database
+    books = Book.objects.all()
+    context = {'book_list':books}
+    return render(request, 'books/book_list.html',context)
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    
