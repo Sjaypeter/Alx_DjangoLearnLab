72c82fe557a051124fc6b20804fde8e059e738b9
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 16:22:41 +0100

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,14 +44,35 @@ def member_view(request):
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request, pk):
+
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
-        else:
-            form = BookForm()
-            return render(request, 'relationship_app/book_form.html', {'form': form})
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book= get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
     
 
 
