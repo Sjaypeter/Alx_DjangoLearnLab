e569e022a632ce67f26e8dd9a5311d146c30256a
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 14:00:15 +0100

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView, ListView
-from.models import Library
+from .models import Library
 # Create your views here.
 
 def book_list(request):
