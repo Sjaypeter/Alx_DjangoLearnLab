d18f1bc5d5b1a6e7f4ec71788afd7c7fcfdebd93
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 13:09:22 +0100

    Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Book:
     title = models.CharField(max_length=200)
