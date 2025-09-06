109015b76d6dd9dd334731eec0c5a62a30cd4cf4
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 15:37:10 +0100

    Django

---
+++ django-models/LibraryProject/relationship_app/passes.py
@@ -0,0 +1,9 @@
+#Utility function to check roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import user_passes_test
+from .passes import is_admin,is_librarian,is_member
 # Create your views here.
 
 def book_list(request):
@@ -26,17 +27,17 @@ class SignUpView(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
     
-    
-@user_passes_test    
+@user_passes_test (is_admin)
 def admin_view(request):
-    return render(request,'admin_view.html')
-@user_passes_test
+    return render(request,'relationship_app/admin_view.html')
+@user_passes_test(is_librarian)
 def librarian_view(request):
-   return render(request,'librarian_view.html') 
-@user_passes_test
+   return render(request,'relationship_app/librarian_view.html') 
+@user_passes_test(is_member)
 def member_view(request):
-    return render(request,'member_view.html')
+    return render(request,'relationship_app/member_view.html')
     
     
 
