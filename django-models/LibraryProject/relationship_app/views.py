32bbe9ae2bbf7d0f902815e491c4448f929e88b9
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 13:49:46 +0100

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,8 @@ from django.views.generic import DetailView, ListView
 def book_list(request):
     #A basic function view that lists all books stored in the database
     books = Book.objects.all()
-    context = {'book_list':books}
-    return render(request, 'books/book_list.html',context)
+    context = {'list_books':books}
+    return render(request, 'list_books.html',context)
 
 
 class LibraryDetailView(DetailView):
