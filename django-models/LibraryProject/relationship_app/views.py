7093b0ffdef3e653e82d932c224f7ef12112dc8a
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-06 14:46:11 +0100

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
     
