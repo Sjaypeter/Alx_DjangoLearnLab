f709db9f8c243bd24ae530e4f3265b3175d094d8
Sjaypeter <Sjaypeter.sjp@gmail.com>
2025-09-29 16:47:14 +0100

    Views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from . models import Post
+from .serializers import PostSerilizer
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 class RegisterView(CreateView):
@@ -17,7 +23,7 @@ class RegisterView(CreateView):
 
 
 
-
+#This creates views that allows authenticated users to view their profile
 @login_required
 def profile_view(request):
     if request.method == "POST":
@@ -46,3 +52,26 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'blog/templates/logout.html')
+
+
+class ListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerilizer
+    
+class DetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerilizer
+    
+class CreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerilizer
+    
+class UpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerilizer
+    
+    
+class DeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerilizer
+    
