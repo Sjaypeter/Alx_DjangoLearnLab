bc7b8b1897938cbcbdee13ba46f8d34d11a196a5
Sjayp <Sjaypeter.sjp@gmail.com>
2025-10-07 20:56:44 +0100

    Api added to urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/posts/', include('posts.urls')),
 ]
